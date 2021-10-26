31c70d808a998740399c5a7dd81740d43bab2964
lisakor <elizaveta.korotkova@gmail.com>
2021-10-26 17:50:07 +0300

    Script fix

--- evaluate_students.sh
+++ evaluate_students.sh
@@ -23,7 +23,7 @@ echo "Held-out test sets"
 echo "STUDENT-0"
 
 STUDENT_PATH=models/student-from-teacher-0
-cd $STUDENT_PATH
+# cd $STUDENT_PATH
 for corpus in Europarl GNOME KDE4 Ubuntu DGT JRC-Acquis EMEA OpenSubtitles infopankki ParaCrawl
 do
   test_src=${TEST_SETS_PATH}/shuf-${corpus}.test.${SRCLANG}
@@ -31,14 +31,15 @@ do
   
   echo $corpus
   cat $test_src \
-  |$MARIAN/marian-decoder --config model.npz.best-bleu-detok.npz.decoder.yml \
+  |$MARIAN/marian-decoder \
+  --config ${STUDENT_PATH}/model.npz.best-bleu-detok.npz.decoder.yml \
   --quiet --log $LOGS_DIR/transl_student_t0_${corpus}.log --devices $GPUS $@ \
   | tee $OUT_PATH/transl-student-t0-$corpus.$TGTLANG \
   | sacrebleu $test_tgt
 done
 
 echo "STUDENT-FT"
-cd $STUDENT_PATH
+# cd $STUDENT_PATH
 STUDENT_PATH=models/student-from-teachers-ft
 for corpus in Europarl GNOME KDE4 Ubuntu DGT JRC-Acquis EMEA OpenSubtitles infopankki ParaCrawl
 do
@@ -47,7 +48,8 @@ do
   
   echo $corpus
   cat $test_src \
-  |$MARIAN/marian-decoder --config model.npz.best-bleu-detok.npz.decoder.yml \
+  |$MARIAN/marian-decoder \
+  \--config ${STUDENT_PATH}/model.npz.best-bleu-detok.npz.decoder.yml \
   --quiet --log $LOGS_DIR/transl_student_tft_${corpus}.log --devices $GPUS $@ \
   | tee $OUT_PATH/transl-student-tft-$corpus.$TGTLANG \
   | sacrebleu $test_tgt
@@ -60,18 +62,20 @@ test_src=${TEST_SETS_PATH}/wmt18.${SRCLANG}-${TGTLANG}.test.src
 test_tgt=${TEST_SETS_PATH}/wmt18.${SRCLANG}-${TGTLANG}.test.ref
 STUDENT_PATH=models/student-from-teacher-0
 echo "STUDENT-0"
-cd $STUDENT_PATH
+# cd $STUDENT_PATH
 cat $test_src \
-|$MARIAN/marian-decoder --config model.npz.best-bleu-detok.npz.decoder.yml \
+|$MARIAN/marian-decoder \
+--config ${STUDENT_PATH}/model.npz.best-bleu-detok.npz.decoder.yml \
 --quiet --log $LOGS_DIR/transl_student_t0_wmt.log --devices $GPUS $@ \
 | tee $OUT_PATH/transl-student-t0-wmt.$TGTLANG \
 | sacrebleu $test_tgt
 
 STUDENT_PATH=models/student-from-teachers-ft
 echo "STUDENT-FT"
-cd $STUDENT_PATH
+# cd $STUDENT_PATH
 cat $test_src \
-|$MARIAN/marian-decoder --config model.npz.best-bleu-detok.npz.decoder.yml \
+|$MARIAN/marian-decoder \
+--config ${STUDENT_PATH}/model.npz.best-bleu-detok.npz.decoder.yml \
 --quiet --log $LOGS_DIR/transl_student_tft_wmt.log --devices $GPUS $@ \
 | tee $OUT_PATH/transl-student-tft-wmt.$TGTLANG \
 | sacrebleu $test_tgt
